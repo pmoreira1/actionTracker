0173fc59370a0b62c80c52ff0ab8de6f439ad51e
Moreira <pedro.moreira@vimn.com>
2018-10-01 23:59:12 +0200

    slim test

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
 //hold all api calls
+use \Psr\Http\Message\ServerRequestInterface as Request;
 
+require 'vendor/autoload.php';
 
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+]);
 
+$app->post("/sleep", function ($request, $response) {
+    $data = $request->getParsedBody();
+    print_r($data);
+});
+
+$app->run();
