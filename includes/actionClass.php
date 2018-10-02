af1beb345fa84e62886444109845fcbb9bc9df75
Moreira <pedro.moreira@vimn.com>
2018-10-02 11:51:31 +0200

    dayENd

--- includes/actionClass.php
+++ includes/actionClass.php
@@ -24,14 +24,27 @@ class ActionTracker
         return $this->db->select($q)[0];
     }
 
+    public function dayEnd($date)
+    {
+        $dayStart = $this->dayStart['timestamp'];
+        $q = "SELECT * from dayActions where `action` = 2 and `dateTime` > '$dayStart' ORDER BY `dateTime` desc limit 1";
+        return $this->db->select($q)[0];
+    }
+
     public function lastActivity($activity)
     {
         $q = "SELECT * from dayActions where `action`= $activity ORDER BY `dateTime` desc limit 1 ";
         return $this->db->select($q)[0];
     }
 
-    public function dayActivity($activity) {
-        $q = "SELECT COUNT(*) as `total` FROM dayActions WHERE `action` = $activity";
+    public function dayActivity($activity, $date = false)
+    {
+        if (!$date) {
+            $date = date('Y-m-d');
+        }
+        $dayStart = $this->dayStart($date);
+        $dayEnd = $this->dayEnd($date);
+        $q = "SELECT COUNT(*) as `total` FROM dayActions WHERE `action` = $activity and `timestamp` BETWEEN " . $this->db->quote($dayStart['dateTime']) . "  and " . $this->db->quote($dayEnd['dateTime']) . "";
         return $this->db->select($q)[0];
     }
 
